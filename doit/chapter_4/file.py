dae71c5b47933b0ecf44922b69d6df1f4384bef9
jhintmain <jhintmain@gmail.com>
2023-05-18 04:24:39 +0900

    20230517_doit_4장_파일

---
+++ doit/chapter_4/file.py
@@ -0,0 +1,27 @@
+# 파일생성하기
+# f = open("새파일.txt",'w')
+# f.close()
+
+# 파일모드 3가지
+# r : 읽기모드
+# w : 쓰기모드
+# a : 추가모드 - 파일 마지막에 내용추가
+
+# 파일생성하기
+import os
+parent_path = os.pardir # ..
+cuurent_path = os.curdir # .
+work_path_full = os.getcwd() # workspace
+now_path_full = os.path.dirname(os.path.realpath(__file__)) # 현재 작업중인 프로젝트
+
+f = open(os.path.join(now_path_full,"새파일.txt"),'w')
+f.close()
+
+
+# 파일만들기 : writedata.py
+f = open(os.path.join(now_path_full,"n번째줄.txt"),'w', encoding="UTF-8")
+
+for i in range(1,11):
+    data = f"{i}번째 줄입니다\n"
+    f.write(data)
+f.close()
---
+++ doit/chapter_4/function.py
@@ -0,0 +1,42 @@
+# 기본값 설정 가능
+def add(a=2, b=4) :
+    return a*b
+
+val = add()
+print(val) # 8
+
+val2 = add(4,4)
+print(val2) # 16
+
+##########################################################
+# 입력값이 몇개일지 모를때
+
+def add_many(*args) :
+    result = 0
+    for num in args : 
+        result += num
+    return result
+
+print(add_many(1,2,3,4,5)) # 1+2+3+4+5 = 15
+
+##########################################################
+# ** 매개변수 dictionary 만들기
+
+def print_kwargs(**kwargs):
+    print(kwargs)
+
+print_kwargs(a=1) # {'a': 1}
+
+print_kwargs(name='foo',age=3) # {'name': 'foo','age':3}
+
+##########################################################
+# 함수의 return값은 언제나 1개
+def add_and_mul(a,b):
+    return a+b, a*b # return이 2개인것처럼 보이지만 , 튜플형태로 1개 return값이다(7,12)
+
+result = add_and_mul(3,4)
+a,b = add_and_mul(3,4)
+print(result) # (7, 12)
+print(a) # 7
+print(b) # 12
+
